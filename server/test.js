48befbe4e7b4c24fc89900d4086fa73f389bbacf
Steven J Raaen <sraaen@gmail.com>
2024-01-16 11:50:09 -0500

    added test file for assistants

--- server/test.js
+++ server/test.js
@@ -1,40 +1,36 @@
 const dotenv = require('dotenv')
 const OpenAI = require("openai")
 const inputs = require('./exampleStrings')
+const MessageContentText = require('openai/resources/beta/threads/messages/messages')
 dotenv.config()
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
 });
 async function xxxx() {
-    const asst =  [
-      {role: "system",content: "You are a helpful assistant designed to output JSON."},
-      // {role: 'user', content: `Read the following text """ ${r}."""`},
-      {role: 'user', content: `Extract words in the text that represent product management skills, technical skills, and sales skills.`},
-      {role: 'user', content: `Format the list as a javascript array.`}
-    ]
+
     const cv = 'file-N7vGQLk49QRscIK81rXHN342'
     const  desc = 'file-YAnUSuq89OJPZzeEW7bNkaEm'
 // ----------------------------- -----------------------------------------------
 
-    const assistant = await openai.beta.assistants.create({
-      name: "Skills Extractor",
-      description: "You will be provided the text of either a resume or a job description. The text will include skills. The skills may or may not be labeled as such. You will extract the skills that could be important in a business context. Each skill will be four words or less.  You will categorize skills e.g. 'programming', 'sales', 'communications', 'management'.",
-      model: "gpt-3.5-turbo-1106",
-      tools: [{"type": "code_interpreter"}],
-      file_ids: [cv, desc]
-    });
-    console.log('Assistant has been created: ', assistant)
+    // const assistant = await openai.beta.assistants.create({
+    //   name: "Skills Extractor",
+    //   description: "You will be provided the text of either a resume or a job description. The text will include skills. The skills may or may not be labeled as such. You will extract the skills that could be important in a business context. Each skill will be four words or less.  You will categorize skills e.g. 'programming', 'sales', 'communications', 'management'.",
+    //   model: "gpt-3.5-turbo-1106",
+    //   tools: [{"type": "code_interpreter"}],
+    //   file_ids: [cv, desc]
+    // });
+    // console.log('Assistant has been created: ', assistant)
 
-    const thread = await openai.beta.threads.create({})
-    console.log('Thred has been created: ', thread)
-
-    const message = await openai.beta.threads.messages.create(thread.id, {
+    // const thread = await openai.beta.threads.create({})
+    // console.log('Thred has been created: ', thread)
+    const skillsThreadID = 'thread_Eoe88ImZpIFsWaHwFlpH2Zi9'
+    const message = await openai.beta.threads.messages.create(skillsThreadID, {
       role: 'user',
       content:
         'Return a javascript array of the skills identified in the file ids.',
     })
-    const run = await openai.beta.threads.runs.create(thread.id, {
-      assistant_id: assistant.id,
+    const run = await openai.beta.threads.runs.create(skillsThreadID, {
+      assistant_id: 'asst_mk6S5OHu9bcYTfqKIATAiIeZ',
     })
     console.log('Run has been created: ', run)
 
@@ -42,7 +38,7 @@ async function xxxx() {
     return new Promise((resolve, reject) => {
       const interval = setInterval(async () => {
         const retrieveRun = await openai.beta.threads.runs.retrieve(
-          thread.id,
+          skillsThreadID,
           run.id
         )
 
@@ -58,7 +54,13 @@ async function xxxx() {
     })
   }
   await checkRun()
+  const messages = await openai.beta.threads.messages.list(skillsThreadID)
+  console.log(messages)
 
+  // for(let i=0; i < messages.data.length; i++) {
+  //   console.log(messages.data[i].content)
+  // }
+  // console.log('line 53' , messages.data.length)
   
 }
 xxxx()
