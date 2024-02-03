0f7f47ab36c1f5aa52e89cde1b86e1022972d531
Steven J Raaen <sraaen@gmail.com>
2024-02-03 18:27:53 -0500

    Setting skills changed to single function

--- server/index.js
+++ server/index.js
@@ -11,57 +11,109 @@ const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
 });
 
-const covLetAsstInstrux = 'I am applying for a job and need a cover letter.  I will provide my resume and a job description. I have several categories of skills, and the cover letter should highlight skills and experiences from my resume that match the job requirements. I may also direct you to use a specific tone, for example, more formal or less formal. I also might ask you to emphasize a certain experience or skill.';
+// const covLetAsstInstrux = 'I am applying for a job and need a cover letter.  I will provide my resume and a job description. I have several categories of skills, and the cover letter should highlight skills and experiences from my resume that match the job requirements. I may also direct you to use a specific tone, for example, more formal or less formal. I also might ask you to emphasize a certain experience or skill.';
 
 
-async function getCL(obj) {
-    const thread = await openai.beta.threads.create({})
-    const message = await openai.beta.threads.messages.create(thread.id, {
-      role: 'user',
-      content:
-        'Return a javascript array of the skills identified in the file ids.',
-    })
-    console.log(obj)
-    const run = await openai.beta.threads.runs.create(thread.id, {
-      assistant_id: 'asst_AUWadjn4T6RXjvX9YA2iMKdo',
-    })
-    console.log('Run has been created: ', run)
-}
-// 
 
-async function getBlurb(r) {
-  console.log(r)
-  const resStats = await openai.chat.completions.create({
-  model: "gpt-3.5-turbo-1106",
-  response_format: { type: "json_object" },
-  messages: [
-  {role: "system",content: "You are a helpful assistant designed to output JSON."},
-  {role: 'user', content: `Read the following text """ ${r}."""`},
-  {role: 'user', content: `Extract words in the text that represent product management skills, technical skills, and sales skills.`},
-  {role: 'user', content: `Format the list as a javascript array.`}
-],
-	  max_tokens: 1000,
-	  temperature: 0
-	});
-  // const stats = await resStats.data
-  // console.log(resStats.choices[0].message)
-	// return stats
-}
+    
+//     const thread = await openai.beta.threads.create({})
+//     const message = await openai.beta.threads.messages.create(thread.id, {
+//       role: 'user',
+//       content:
+//         'Return a javascript array of the skills identified in the file ids.',
+//     })
+//     console.log(obj)
+//     const run = await openai.beta.threads.runs.create(thread.id, {
+//       assistant_id: 'asst_AUWadjn4T6RXjvX9YA2iMKdo',
+//     })
+//     console.log('Run has been created: ', run)
+// }
+// // 
+// async function getBlurb(r) {}
+
+
+
 app.post('/api/blurb', async(req, res) => {
-    res.send(await getBlurb(req.body.params[0].resume))
+  console.log(req.body.params[0].doc)
+//   const resumeSkills = await openai.chat.completions.create({
+//   model: "gpt-3.5-turbo-1106",
+//   response_format: { type: "json_object" },
+//   messages: [
+//   {role: "system",content: "You are a helpful assistant designed to output JSON."},
+//   {role: 'user', content: `Read the following text """ ${r}."""`},
+//   {role: 'user', content: `Extract words in the text that represent product management skills, technical skills, and sales skills.`},
+//   {role: 'user', content: `Format the list as a javascript array.`}
+// ],
+//     max_tokens: 1000,
+//     temperature: 0
+//   });
+//   const skillsList = await resumeSkills.data
+//   return skillsList
+
+//     res.send(await getBlurb(req.body.params[0].resume))
 })
+
+
 app.post('/api/cvWriter', async(req, res) => {
-  console.log(res)
-    res.send(await getCL({'resume':req.body.params[0].resume, 'description':req.body.params[0].description}))
+  const resume = req.body.params[0].obj.resume
+  const description = req.body.params[0].obj.description
+
+  const thread = await openai.beta.threads.create({})
+  const message = await openai.beta.threads.messages.create(thread.id, {
+    role: 'user',
+    content:
+      `${resume} is a string preseenting my resume. ${description} is a string representing the job I am applying to. Write a 150 word cover letter that 
+      points out how my skills and experience match the job description`,
+      stream: true
+  })
+
+  const run = await openai.beta.threads.runs.create(thread.id, {
+    assistant_id: 'asst_AUWadjn4T6RXjvX9YA2iMKdo',
+  })
+  console.log('Run has been created: ', run)
+  const checkRun = async () => {
+    return new Promise((resolve, reject) => {
+      const interval = setInterval(async () => {
+        const retrieveRun = await openai.beta.threads.runs.retrieve(
+          thread.id,
+          run.id
+        )
+
+        console.log('Run status: ', retrieveRun.status)
+        if (retrieveRun.status === 'completed') {
+          console.log('Run completed: ')
+          clearInterval(interval)
+          resolve(retrieveRun)
+        }
+      }, 3000)
+    })
+  }
+  await checkRun()
+    const messages = await openai.beta.threads.messages.list(thread.id)
+    console.log(messages.data[0].content[0].text)
+
+  // for(let i=0; i < messages.data.length; i++) {
+  //   console.log(messages.data[i].content)
+  // }
+
+    res.json(messages.data[0].content[0].text)
 })
 app.listen(port, () => {
   console.log(`App listening on port ${port}`)
 })
 
+// async function main() {
+//   const stream = await openai.chat.completions.create({
+//     model: 'gpt-4',
+//     messages: [{ role: 'user', content: 'Tell me a knock knockjoke' }],
+//     stream: true,
+//   });
+//   for await (const chunk of stream) {
+//     process.stdout.write(chunk.choices[0]?.delta?.content || '');
+//   }
+// }
 
-
-
-
+// main();
 
 
 
--- server/test.js
+++ server/test.js
@@ -1,66 +1,80 @@
 const dotenv = require('dotenv')
 const OpenAI = require("openai")
 // const inputs = require('./exampleStrings')
-const MessageContentText = require('openai/resources/beta/threads/messages/messages')
+// const MessageContentText = require('openai/resources/beta/threads/messages/messages')
 dotenv.config()
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
 });
-async function xxxx() {
+// async function xxxx() {
 
-    const cv = 'file-N7vGQLk49QRscIK81rXHN342'
-    const  desc = 'file-YAnUSuq89OJPZzeEW7bNkaEm'
-// ----------------------------- -----------------------------------------------
+//     const cv = 'file-N7vGQLk49QRscIK81rXHN342'
+//     const  desc = 'file-YAnUSuq89OJPZzeEW7bNkaEm'
+// // ----------------------------- -----------------------------------------------
 
-    // const assistant = await openai.beta.assistants.create({
-    //   name: "Skills Extractor",
-    //   description: "You will be provided the text of either a resume or a job description. The text will include skills. The skills may or may not be labeled as such. You will extract the skills that could be important in a business context. Each skill will be four words or less.  You will categorize skills e.g. 'programming', 'sales', 'communications', 'management'.",
-    //   model: "gpt-3.5-turbo-1106",
-    //   tools: [{"type": "code_interpreter"}],
-    //   file_ids: [cv, desc]
-    // });
-    // console.log('Assistant has been created: ', assistant)
+//     const assistant = await openai.beta.assistants.create({
+//       name: "Skills Extractor",
+//       description: "You will be provided the text of either a resume or a job description. The text will include skills. The skills may or may not be labeled as such. You will extract the skills that could be important in a business context. Each skill will be four words or less.  You will categorize skills e.g. 'programming', 'sales', 'communications', 'management'.",
+//       model: "gpt-3.5-turbo-1106",
+//       tools: [{"type": "code_interpreter"}],
+//       file_ids: [cv, desc]
+//     });
+//     console.log('Assistant has been created: ', assistant)
 
-    // const thread = await openai.beta.threads.create({})
-    // console.log('Thred has been created: ', thread)
-    const skillsThreadID = 'thread_Eoe88ImZpIFsWaHwFlpH2Zi9'
-    const message = await openai.beta.threads.messages.create(skillsThreadID, {
-      role: 'user',
-      content:
-        'Return a javascript array of the skills identified in the file ids.',
-    })
-    const run = await openai.beta.threads.runs.create(skillsThreadID, {
-      assistant_id: 'asst_mk6S5OHu9bcYTfqKIATAiIeZ',
-    })
-    console.log('Run has been created: ', run)
+//     const thread = await openai.beta.threads.create({})
+//     console.log('Thred has been created: ', thread)
+//     const letterThreadID = 'thread_Eoe88ImZpIFsWaHwFlpH2Zi9'
 
-  const checkRun = async () => {
-    return new Promise((resolve, reject) => {
-      const interval = setInterval(async () => {
-        const retrieveRun = await openai.beta.threads.runs.retrieve(
-          skillsThreadID,
-          run.id
-        )
+//     const skillsThreadID = 'thread_Eoe88ImZpIFsWaHwFlpH2Zi9'
+//     const message = await openai.beta.threads.messages.create(skillsThreadID, {
+//       role: 'user',
+//       content:
+//         'Return a javascript array of the skills identified in the file ids.',
+//     })
+//     const run = await openai.beta.threads.runs.create(skillsThreadID, {
+//       assistant_id: 'asst_mk6S5OHu9bcYTfqKIATAiIeZ',
+//     })
+//     console.log('Run has been created: ', run)
 
-        console.log('Run status: ', retrieveRun.status)
+//   const checkRun = async () => {
+//     return new Promise((resolve, reject) => {
+//       const interval = setInterval(async () => {
+//         const retrieveRun = await openai.beta.threads.runs.retrieve(
+//           skillsThreadID,
+//           run.id
+//         )
 
-        if (retrieveRun.status === 'completed') {
-          console.log('Run completed: ', retrieveRun)
+//         console.log('Run status: ', retrieveRun.status)
 
-          clearInterval(interval)
-          resolve(retrieveRun)
-        }
-      }, 3000)
-    })
-  }
-  await checkRun()
-  const messages = await openai.beta.threads.messages.list(skillsThreadID)
-  console.log(messages)
+//         if (retrieveRun.status === 'completed') {
+//           console.log('Run completed: ', retrieveRun)
 
-  // for(let i=0; i < messages.data.length; i++) {
-  //   console.log(messages.data[i].content)
-  // }
-  // console.log('line 53' , messages.data.length)
+//           clearInterval(interval)
+//           resolve(retrieveRun)
+//         }
+//       }, 3000)
+//     })
+//   }
+//   await checkRun()
+//   const messages = await openai.beta.threads.messages.list(skillsThreadID)
+//   console.log(messages)
+
+//   for(let i=0; i < messages.data.length; i++) {
+//     console.log(messages.data[i].content)
+//   }
+//   console.log('line 53' , messages.data.length)
   
+// }
+// xxxx()
+async function main() {
+  const stream = await openai.chat.completions.create({
+    model: 'gpt-4',
+    messages: [{ role: 'user', content: 'Tell me a knock knockjoke' }],
+    stream: true,
+  });
+  for await (const chunk of stream) {
+    process.stdout.write(chunk.choices[0]?.delta?.content || '');
+  }
 }
-xxxx()
+
+main();
